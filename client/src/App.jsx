88fb82711fdef864c25fb71dc33ff01185620634
Parth Shah <shahpn02@gmail.com>
2025-07-12 14:19:18 -0400

    Revert "Adjustments to style"
    This reverts commit 82044230ea5d7f1ce9f16f12038123d09be87382.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -34,15 +34,7 @@ function App() {
           <img
             src="https://developer.spotify.com/images/guidelines/design/icon3@2x.png"
             alt="Spotify logo"
-            style={{
-              height: 22,
-              verticalAlign: "middle",
-              marginRight: 10,
-              marginBottom: 3,
-              background: "white",
-              borderRadius: "50%",
-              padding: 2,
-            }}
+            style={{ height: 22, verticalAlign: 'middle', marginRight: 10 }}
           />
           Login with Spotify
         </button>
