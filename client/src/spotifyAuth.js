0f57b7d3ea3f92229183806300d3ea631c31fa9d
Parth Shah <shahpn02@gmail.com>
2025-07-12 17:50:15 -0400

    Debug Login Issue pt.5

--- client/src/spotifyAuth.js
+++ client/src/spotifyAuth.js
@@ -1,19 +1,26 @@
 import pkceChallenge from "pkce-challenge";
 
 export function getSpotifyLoginUrl(clientId, redirectUri, scope) {
-  const { code_challenge, code_verifier } = pkceChallenge();
-  console.log("Generated code_verifier (login):", code_verifier);
-  localStorage.setItem("spotify_pkce_verifier", code_verifier);
+  // Generate PKCE challenge/verifier
+  const pkce = pkceChallenge(); // <-- Don't destructure, use pkce.code_challenge and pkce.code_verifier
 
+  // Sanity check
+  if (!pkce.code_challenge || !pkce.code_verifier) {
+    throw new Error("Failed to generate PKCE challenge/verifier!");
+  }
+
+  // Save verifier in localStorage
+  localStorage.setItem("spotify_pkce_verifier", pkce.code_verifier);
+
+  // Build auth URL
   const params = new URLSearchParams({
-    response_type: "code" ,
-    client_id: clientId, 
-    scope,
+    response_type: "code",
+    client_id: clientId,
     redirect_uri: redirectUri,
+    scope,
     code_challenge_method: "S256",
-    code_challenge,
+    code_challenge: pkce.code_challenge,
   });
 
   return `https://accounts.spotify.com/authorize?${params.toString()}`;
-
 }
